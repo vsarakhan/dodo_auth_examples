673a9bbbafb90e00ce0ad946eb5dffc18910c2aa
“Vlad <v.sarakhan@dodopizza.com>
2024-04-15 15:14:30 +0300

    add comments

--- auth_code_flow_example/app.py
+++ auth_code_flow_example/app.py
@@ -3,40 +3,48 @@ import requests as http
 import os
 import webbrowser
 
-__client_id__ = "<your_client_id_here>"
-__client_secret__ = os.getenv('CLIENT_SECRET')
-__code_challenge__ = "51FaJvQFsiNdiFWIq2EMWUKeAqD47dqU_cHzJpfHl-Q"
+#client id and client secret from https://marketplace.dodois.io/dev/apps
+__client_id__ = "<your_client_id_here>" #pass your client id here
+__client_secret__ = os.getenv('CLIENT_SECRET') #get your client secret from environment variable 
+                                                #we do not recommend storing the secret directly in code
+
+#PKCE for oauth2. you can read the details here https://auth0.com/docs/get-started/authentication-and-authorization-flow/authorization-code-flow-with-pkce
+#for example we predefine codes for PKCE
+__code_challenge__ = "51FaJvQFsiNdiFWIq2EMWUKeAqD47dqU_cHzJpfHl-Q" 
 __code_verifier__ = "c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646"
-__scopes__ = { "openid", "shared" }
-__redirect_url__ = "https://localhost:5001"
+
+__scopes__ = { "openid", "shared" } # your client scopes. you can find then https://marketplace.dodois.io/dev/apps
+__redirect_url__ = "https://localhost:5001" # your app redirect url. by default `https://localhost:5001`
 
 app = Flask(__name__)
 
-@app.route("/")
+# oauth2 code flow details https://auth0.com/docs/get-started/authentication-and-authorization-flow/authorization-code-flow
+@app.route("/") #endpoint for 5 step from the link above
 def get_token():
     args = request.args
-    code = args.get("code", default="", type=str)
+    code = args.get("code", default="", type=str) #get code from query param
 
-    request_body = {
+    request_body = { #build request body for step 6
         "client_id": __client_id__,
         "client_secret": __client_secret__,
         "code_verifier": __code_verifier__,
         "scope":" ".join(__scopes__),
-        "grant_type": "authorization_code",
+        "grant_type": "authorization_code", #indicate that we use authorization code flow 
         "redirect_uri": __redirect_url__,
         "code": code
     }
-    auth_result = http.post("https://auth.dodois.io/connect/token", data=request_body)
+    auth_result = http.post("https://auth.dodois.io/connect/token", data=request_body)  #send request for token (step 6)
 
-    access_token = auth_result.json()["access_token"]
-    save_token(access_token)
+    access_token = auth_result.json()["access_token"] #get access token from response 
+    save_token(access_token) #do what you want with this token
 
-    return 'Success', 200
+    return 'Success', 200 #success result
 
 def save_token(token):
     print(token)
     #do what you need
 
+#build user authorization url and open it in browser
 def show_sign_in_page(clientId, scopes, redirect_url, code_challenge):
     print("Open this link in browser:")
     url = f'https://auth.dodois.io/connect/authorize?client_id={clientId}&scope={" ".join(scopes)}&response_type=code&redirect_uri={redirect_url}&code_challenge={code_challenge}&code_challenge_method=S256'
@@ -44,6 +52,6 @@ def show_sign_in_page(clientId, scopes, redirect_url, code_challenge):
     webbrowser.open(url)
 
 if __name__ == "__main__":
-    show_sign_in_page(__client_id__, __scopes__, __redirect_url__, __code_challenge__)
-    app.run(host="localhost", port=5001, ssl_context='adhoc')
+    show_sign_in_page(__client_id__, __scopes__, __redirect_url__, __code_challenge__) #open consent
+    app.run(host="localhost", port=5001, ssl_context='adhoc') #run server on https://localhost:5001
 
